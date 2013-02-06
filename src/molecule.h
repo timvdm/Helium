f598341aa69cca2b883d8efb9ad528b1b9e6f160
timvdm <tim.vandermeersch@gmail.com>
2013-02-06 19:24:37 +0100

    molecule.h: Exclude form docs

--- src/molecule.h
+++ src/molecule.h
@@ -1,3 +1,29 @@
+/**
+ * Copyright (c) 2013, Tim Vandermeersch
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #ifndef HELIUM_MOLECULE_H
 #define HELIUM_MOLECULE_H
 
@@ -5,8 +31,18 @@
 
 namespace Helium {
 
+  /**
+   * Type used for atom and bond indices.
+   */
   typedef unsigned int Index;
+  /**
+   * Type used for various sizes (number of atoms/bonds, number of neighbors,
+   * ...).
+   */
   typedef unsigned int Size;
+
+  //@cond dev
+  //@{
 
   template<typename MoleculeType>
   struct molecule_traits
@@ -18,8 +54,9 @@ namespace Helium {
     typedef typename MoleculeType::mol_bond_iter mol_bond_iter;
     typedef typename MoleculeType::atom_atom_iter atom_atom_iter;
     typedef typename MoleculeType::atom_bond_iter atom_bond_iter;
-    
+
     typedef typename MoleculeType::const_atom_iter const_atom_iter;
+    typedef typename MoleculeType::const_bond_iter const_bond_iter;
 
     static Index null_index()
     {
@@ -144,6 +181,8 @@ namespace Helium {
 
   template<typename MoleculeType>
   bool get_order(const MoleculeType *mol, typename molecule_traits<MoleculeType>::bond_type bond);
+
+  //@}
 
 }
 

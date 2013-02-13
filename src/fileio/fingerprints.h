ef7c85707027fe3225458350e7008c463dc95b29
timvdm <tim.vandermeersch@gmail.com>
2013-02-13 16:57:22 +0100

    Add 'transpose' tool to convert row-major to column-major order fingerprint files.

--- src/fileio/fingerprints.h
+++ src/fileio/fingerprints.h
@@ -1,3 +1,29 @@
+/**
+ * Copyright (c) 2013, Tim Vandermeersch
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #ifndef HELIUM_FILEIO_FINGERPRINTS_H
 #define HELIUM_FILEIO_FINGERPRINTS_H
 
@@ -221,8 +247,8 @@ query: 00001011 (bit 5, 7 & 8 are set)
    *
    */
 
-   
-   
+
+
   class RowMajorFingerprintOutputFile
   {
     public:
@@ -244,6 +270,53 @@ query: 00001011 (bit 5, 7 & 8 are set)
     private:
       BinaryOutputFile m_file;
       unsigned int m_numBytes;
+  };
+
+  class ColumnMajorFingerprintOutputFile
+  {
+    public:
+      ColumnMajorFingerprintOutputFile(const std::string &filename, unsigned int numBits,
+          unsigned int numFingerprints) : m_file(filename), m_numBits(numBits),
+          m_numFingerprints(numFingerprints), m_current(0)
+      {
+        // allocate data
+        m_data = new Word[bitvec_num_words_for_bits(numFingerprints) * numBits];
+      }
+
+      ~ColumnMajorFingerprintOutputFile()
+      {
+        // write the data
+        m_file.write(m_data, bitvec_num_words_for_bits(m_numFingerprints) * m_numBits * sizeof(Word));
+        delete [] m_data;
+      }
+
+      bool writeFingerprint(Word *fingerprint)
+      {
+        // check each bit in the fingerprint
+        for (int i = 0; i < m_numBits; ++i) {
+          // skip this bit if it is not set
+          if (!bitvec_get(i, fingerprint))
+            continue;
+
+          // set the correct bit
+          bitvec_set(i * bitvec_num_words_for_bits(m_numFingerprints) * 8 * sizeof(Word) + m_current, m_data);
+        }
+        ++m_current;
+
+        return true;
+      }
+
+      bool writeHeader(const std::string &header)
+      {
+        return m_file.writeHeader(header);
+      }
+
+    private:
+      BinaryOutputFile m_file;
+      unsigned int m_numBits;
+      unsigned int m_numFingerprints;
+      unsigned int m_current;
+      Word *m_data;
   };
 
   class InMemoryRowMajorFingerprintStorage
@@ -300,14 +373,16 @@ query: 00001011 (bit 5, 7 & 8 are set)
         }
 
         // make sure the required attributes are present
-        if (!data.isMember("num_bits")) {
+        if (!data.isMember("order"))
+          throw std::runtime_error(make_string("JSON header for file ", filename, " does not contain 'order' attribute"));
+        if (!data.isMember("num_bits"))
           throw std::runtime_error(make_string("JSON header for file ", filename, " does not contain 'num_bits' attribute"));
-          return;
-        }
-        if (!data.isMember("num_fingerprints")) {
+        if (!data.isMember("num_fingerprints"))
           throw std::runtime_error(make_string("JSON header for file ", filename, " does not contain 'num_fingerprints' attribute"));
-          return;
-        }
+
+        // ensure this is a row-major order file
+        if (data["order"].asString() != "row-major")
+          throw std::runtime_error(make_string(filename, " is not a row-major order fingerprint file"));
 
         // get attributes from header
         m_numBits = data["num_bits"].asUInt();
@@ -324,261 +399,86 @@ query: 00001011 (bit 5, 7 & 8 are set)
       unsigned int m_numFingerprints;
   };
 
-
-
-
-
-  class FingerprintFile
+  class InMemoryColumnMajorFingerprintStorage
   {
     public:
-      FingerprintFile(const std::string &filename) : m_ifs(filename.c_str()), m_current(-1)
+      InMemoryColumnMajorFingerprintStorage(const std::string &filename)
       {
-        if (m_ifs)
-          read32(m_ifs, m_numFingerprints);    
-        m_numWords = 16;
+        TIMER("Loading InMemoryColumnMajorFingerprintStorage:");
+        load(filename);
       }
 
-      unsigned int num_fingerprints() const
+      ~InMemoryColumnMajorFingerprintStorage()
+      {
+        delete [] m_fingerprints;
+      }
+
+      std::string header() const
+      {
+        return m_json;
+      }
+
+      unsigned int numBits() const
+      {
+        return m_numBits;
+      }
+
+      unsigned int numFingerprints() const
       {
         return m_numFingerprints;
       }
 
-      unsigned int current() const
+      Word* bit(unsigned int index) const
       {
-        return m_current;
-      }
-
-      bool read_fingerprint(Word *fingerprint)
-      {
-        if (!m_ifs)
-          return false;
-        ++m_current;
-        if (m_current == m_numFingerprints)
-          return false;
-        for (int i = 0; i < m_numWords; ++i)
-          read64(m_ifs, fingerprint[i]);
-        return m_ifs;
+        return m_fingerprints + bitvec_num_words_for_bits(m_numFingerprints) * index;
       }
 
     private:
-      std::ifstream m_ifs;
-      unsigned int m_numFingerprints;
-      unsigned int m_current;
-      int m_numWords;
-  };
-
-  struct InvertedFingerprintFileHeader
-  {
-    InvertedFingerprintFileHeader() : magic_number(get_magic_number())
-    {
-    }
-
-    static unsigned int get_magic_number()
-    {
-      return 0x48650001;
-    }
-
-    unsigned int magic_number;
-    unsigned int bits_per_word;
-    unsigned int bits_per_fingerprint;
-    unsigned int words_per_fingerprint;
-    unsigned int words_per_fpbit;
-    unsigned int num_fingerprints;
-  };
-
-  class InvertedFingerprintOutputFile
-  {
-    public:
-      InvertedFingerprintOutputFile(unsigned int bitsPerFingerprint, unsigned int numFingerprints, const std::string &filename) 
-          : m_ofs(filename.c_str(), std::ios_base::out | std::ios_base::binary), m_current(0)
-      {
-        // setup m_header
-        m_header.bits_per_word = sizeof(Word) * 8; // e.g. 64
-        m_header.bits_per_fingerprint = bitsPerFingerprint; // e.g. 1024
-        m_header.words_per_fingerprint = m_header.bits_per_fingerprint / m_header.bits_per_word; // e.g. 16
-        m_header.words_per_fpbit = (numFingerprints + numFingerprints % m_header.bits_per_word) / m_header.bits_per_word;
-        m_header.num_fingerprints = numFingerprints;
-
-        // write m_header
-        if (!m_ofs)
-          throw std::runtime_error(make_string("Could not open ", filename, " for writing."));
-        
-        // write the header
-        m_ofs.write(reinterpret_cast<const char*>(&m_header), sizeof(InvertedFingerprintFileHeader));
-        // allocate data
-        m_data = new Word[m_header.words_per_fpbit * m_header.bits_per_fingerprint];
-      }
-
-      ~InvertedFingerprintOutputFile()
-      {
-        // write the data
-        m_ofs.write(reinterpret_cast<const char*>(m_data), m_header.words_per_fpbit * m_header.bits_per_fingerprint * sizeof(Word));
-        delete [] m_data;
-      }
-
-      void write(Word *fingerprint)
-      {
-        // check each bit in the fingerprint
-        for (int i = 0; i < m_header.bits_per_fingerprint; ++i) {
-          // skip this bit if it is not set
-          if (!bitvec_get(i, fingerprint))
-            continue;
-
-          // set the correct bit
-          bitvec_set(i * m_header.words_per_fpbit * 64 + m_current, m_data);
-        }
-        ++m_current;
-      }
-
-    private:
-      InvertedFingerprintFileHeader m_header;
-      std::ofstream m_ofs;
-      unsigned int m_current;
-      Word *m_data;
-  };
-
-  class InvertedFingerprintFile
-  {
-    public:
-      InvertedFingerprintFile(const std::string &filename) : m_ifs(filename.c_str(), std::ios_base::in | std::ios_base::binary)
-      {
-        if (!m_ifs)
-          throw std::runtime_error(make_string("Could not open ", filename, " for reading."));
-
-        // read the header
-        m_ifs.read(reinterpret_cast<char*>(&m_header), sizeof(InvertedFingerprintFileHeader));
-
-        // check magic number
-        if (m_header.magic_number != m_header.get_magic_number())
-          throw std::runtime_error(make_string(filename, " is not an inverted fingerprint file."));
-
-        // allocate memory
-        m_data = new Word[m_header.words_per_fpbit];
-      }
-
-      ~InvertedFingerprintFile()
-      {
-        delete [] m_data;
-      }
-
-      unsigned int num_fingerprints() const
-      {
-        return m_header.num_fingerprints;
-      }
-
-      Word* allocate_result() const
-      {
-        return new Word[m_header.words_per_fpbit];
-      }
-
-      void search(Word *fingerprint, Word *result)
-      {
-        bool first = true;
-        for (int i = 0; i < m_header.bits_per_fingerprint; ++i) { // foreach bit
-          // skip this bit if it is not set
-          if (!bitvec_get(i, fingerprint))
-            continue;
-        
-
-          // compute offset for this fingerprint bit
-          unsigned int offset = i * m_header.words_per_fpbit;
-          
-          m_ifs.seekg(sizeof(InvertedFingerprintFileHeader) + offset * sizeof(Word));
-          m_ifs.read(reinterpret_cast<char*>(m_data), m_header.words_per_fpbit * sizeof(Word));
-
-          if (first) {
-            // if this is the first bit, just set result
-            for (unsigned int j = 0; j < m_header.words_per_fpbit; ++j)
-              result[j] = m_data[j];
-            first = false;
-          } else {
-            // do set intersection
-            for (unsigned int j = 0; j < m_header.words_per_fpbit; ++j)
-              result[j] &= m_data[j];
-          }
-        }
-      }
-
-    private:
-      InvertedFingerprintFileHeader m_header;
-      std::ifstream m_ifs;
-      Word *m_data;
-  };
-
-  class InvertedFingerprintFileCached
-  {
-    public:
-      InvertedFingerprintFileCached(const std::string &filename)
+      void load(const std::string &filename)
       {
         // open the file
-        std::ifstream ifs(filename.c_str(), std::ios_base::in | std::ios_base::binary);
-        if (!ifs)
-          throw std::runtime_error(make_string("Could not open ", filename, " for reading."));
+        BinaryInputFile file(filename);
+        if (!file) {
+          throw std::runtime_error(make_string("Could not open fingerprint file ", filename));
+          return;
+        }
 
-        // read the header
-        ifs.read(reinterpret_cast<char*>(&m_header), sizeof(InvertedFingerprintFileHeader));
+        // parse the JSON header
+        m_json = file.header();
+        Json::Reader reader;
+        Json::Value data;
+        if (!reader.parse(m_json, data)) {
+          throw std::runtime_error(reader.getFormattedErrorMessages());
+          return;
+        }
 
-        // check magic number
-        if (m_header.magic_number != m_header.get_magic_number())
-          throw std::runtime_error(make_string(filename, " is not an inverted fingerprint file."));
+        // make sure the required attributes are present
+        if (!data.isMember("order"))
+          throw std::runtime_error(make_string("JSON header for file ", filename, " does not contain 'order' attribute"));
+        if (!data.isMember("num_bits"))
+          throw std::runtime_error(make_string("JSON header for file ", filename, " does not contain 'num_bits' attribute"));
+        if (!data.isMember("num_fingerprints"))
+          throw std::runtime_error(make_string("JSON header for file ", filename, " does not contain 'num_fingerprints' attribute"));
+
+        // ensure this is a row-major order file
+        if (data["order"].asString() != "column-major")
+          throw std::runtime_error(make_string(filename, " is not a row-major order fingerprint file"));
+
+        // get attributes from header
+        m_numBits = data["num_bits"].asUInt();
+        m_numFingerprints = data["num_fingerprints"].asUInt();
 
         // allocate memory
-        m_data = new Word[m_header.words_per_fpbit * m_header.bits_per_fingerprint];
-        // read the data
-        ifs.read(reinterpret_cast<char*>(m_data), m_header.words_per_fpbit * m_header.bits_per_fingerprint * sizeof(Word));
+        m_fingerprints = new Word[bitvec_num_words_for_bits(m_numBits) * m_numFingerprints];
+        file.read(m_fingerprints, bitvec_num_words_for_bits(m_numBits) * m_numFingerprints * sizeof(Word));
       }
 
-      ~InvertedFingerprintFileCached()
-      {
-        delete [] m_data;
-      }
-
-      unsigned int num_fingerprints() const
-      {
-        return m_header.num_fingerprints;
-      }
-
-      Word* allocate_fingerprint() const
-      {
-        return new Word[m_header.words_per_fingerprint];
-      }
-      
-      Word* allocate_result() const
-      {
-        return new Word[m_header.words_per_fpbit];
-      }
-
-      void search(Word *fingerprint, Word *result)
-      {
-        bool first = true;
-        for (int i = 0; i < m_header.bits_per_fingerprint; ++i) { // foreach bit
-          // skip this bit if it is not set
-          if (!bitvec_get(i, fingerprint))
-            continue;
-
-          // compute offset for this fingerprint bit
-          unsigned int offset = i * m_header.words_per_fpbit;
-
-          if (first) {
-            // if this is the first bit, just set result
-            for (unsigned int j = 0; j < m_header.words_per_fpbit; ++j)
-              result[j] = m_data[offset + j];
-            first = false;
-          } else {
-            // do set intersection
-            for (unsigned int j = 0; j < m_header.words_per_fpbit; ++j)
-              result[j] &= m_data[offset + j];
-          }
-        }
-      }
-
-    private:
-      InvertedFingerprintFileHeader m_header;
-      Word *m_data;
+      std::string m_json; //!< JSON header
+      Word *m_fingerprints;
+      unsigned int m_numBits;
+      unsigned int m_numFingerprints;
   };
 
-
-  
 }
 
 #endif

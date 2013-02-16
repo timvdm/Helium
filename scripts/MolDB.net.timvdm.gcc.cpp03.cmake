1ac1c93a97994d0c8f59b03fd670611add71ab7f
timvdm <tim.vandermeersch@gmail.com>
2013-02-16 13:08:52 +0100

    Update CDash scripts

--- scripts/MolDB.net.timvdm.gcc.cpp03.cmake
+++ scripts/MolDB.net.timvdm.gcc.cpp03.cmake
@@ -6,7 +6,7 @@ set(CTEST_CVS_UPDATE_OPTIONS "pull")
 #set(CTEST_CVS_CHECKOUT "${CTEST_CVS_COMMAND} clone git://github.com/cryos/avogadro.git \"${CTEST_SOURCE_DIRECTORY}\"")
 
 # which ctest command to use for running the dashboard
-set(CTEST_COMMAND "/usr/bin/ctest -D Experimental")
+set(CTEST_COMMAND "/usr/bin/ctest -D Nightly")
 
 # what cmake command to use for configuring this dashboard
 set(CTEST_CMAKE_COMMAND "/usr/bin/cmake")
@@ -22,9 +22,8 @@ set(CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
 # this is the initial cache to use for the binary tree, be careful to escape
 # any quotes inside of this string if you use it
 set(CTEST_INITIAL_CACHE "CMAKE_CXX_COMPILER:STRING=g++
-ENABLE_CPP11:BOOL=True
-BUILDNAME:STRING=g++ on Ubuntu linux (C++11)
-SITE:STRING=MolDB.org.timvdm
+BUILDNAME:STRING=g++ on Ubuntu linux (C++03)
+SITE:STRING=MolDB.net.timvdm
 CVSCOMMAND:FILEPATH=/usr/bin/git
 MAKECOMMAND:STRING=make -j5
 ")

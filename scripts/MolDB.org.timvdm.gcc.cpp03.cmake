01ea489428c5235967ec5e0d7574e5ced7522102
timvdm <tim.vandermeersch@gmail.com>
2013-02-16 02:35:50 +0100

    Add CDash scripts for MolDB.org site

---
+++ scripts/MolDB.org.timvdm.gcc.cpp03.cmake
@@ -0,0 +1,32 @@
+set(CTEST_SOURCE_DIRECTORY "/home/tim/Cheminformatics/Helium")
+set(CTEST_BINARY_DIRECTORY "/home/tim/Cheminformatics/Helium/build-cdash")
+
+set(CTEST_UPDATE_COMMAND "/usr/bin/git")
+set(CTEST_CVS_UPDATE_OPTIONS "pull")
+#set(CTEST_CVS_CHECKOUT "${CTEST_CVS_COMMAND} clone git://github.com/cryos/avogadro.git \"${CTEST_SOURCE_DIRECTORY}\"")
+
+# which ctest command to use for running the dashboard
+set(CTEST_COMMAND "/usr/bin/ctest -D Experimental")
+
+# what cmake command to use for configuring this dashboard
+set(CTEST_CMAKE_COMMAND "/usr/bin/cmake")
+
+####################################################################
+# The values in this section are optional you can either
+# have them or leave them commented out
+####################################################################
+
+# should ctest wipe the binary tree before running
+set(CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
+
+# this is the initial cache to use for the binary tree, be careful to escape
+# any quotes inside of this string if you use it
+set(CTEST_INITIAL_CACHE "CMAKE_CXX_COMPILER:STRING=g++
+BUILDNAME:STRING=g++ on Ubuntu linux (C++03)
+SITE:STRING=MolDB.org.timvdm
+CVSCOMMAND:FILEPATH=/usr/bin/git
+MAKECOMMAND:STRING=make -j5
+")
+
+# set any extra envionment varibles here
+set(CTEST_ENVIRONMENT)

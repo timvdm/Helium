01a9eb0195c4c45101658a08ba21a325aee76df6
timvdm <tim.vandermeersch@gmail.com>
2013-10-03 13:43:23 +0200

    Add documentation for dfs.h

--- src/algorithms/dfs.h
+++ src/algorithms/dfs.h
@@ -1,3 +1,29 @@
+/*
+ * Copyright (c) 2013, Tim Vandermeersch
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #ifndef HELIUM_DFS_H
 #define HELIUM_DFS_H
 
@@ -7,17 +33,76 @@
 
 namespace Helium {
 
+  /**
+   * @brief Base class for DFS functors.
+   */
   template<typename MoleculeType>
   struct DFSVisitor
   {
+    /**
+     * @brief The molecule type.
+     */
     typedef MoleculeType molecule_type;
+    /**
+     * @brief The atom type.
+     */
     typedef typename molecule_traits<MoleculeType>::atom_type atom_type;
+    /**
+     * @brief The bond type.
+     */
     typedef typename molecule_traits<MoleculeType>::bond_type bond_type;
 
+    /**
+     * @brief Initialize the functor.
+     *
+     * This function is called once when the DFS search is started.
+     *
+     * @param mol The molecule.
+     */
     void initialize(MoleculeType &mol) {}
+
+    /**
+     * @brief Visit an atom.
+     *
+     * This function is called every time a new atom is traversed.
+     *
+     * @param mol The molecule.
+     * @param prev The previous atom on the DFS path.
+     * @param atom The traversed atom.
+     */
     void atom(MoleculeType &mol, atom_type prev, atom_type atom) {}
+
+    /**
+     * @brief Visit a bond.
+     *
+     * This function is called every time a new bond is traversed.
+     *
+     * @param mol The molecule.
+     * @param prev The previous atom on the DFS path.
+     * @param bond The traversed bond.
+     */
     void bond(MoleculeType &mol, atom_type prev, bond_type bond) {}
+
+    /**
+     * @brief Backtrack.
+     *
+     * This function is called when the current path is exhausted and the DFS
+     * search needs to backtrack.
+     *
+     * @param mol The molecule.
+     * @param atom The atom.
+     */
     void backtrack(MoleculeType &mol, atom_type atom) {}
+
+    /**
+     * @brief Visit a "back" bond.
+     *
+     * The "back" bonds are the chords (or ring closure bonds) that would make
+     * the DFS spanning graph cyclic.
+     *
+     * @param mol The molecule.
+     * @param bond The "back" bond.
+     */
     void back_bond(MoleculeType &mol, bond_type bond) {}
   };
 
@@ -63,6 +148,18 @@ namespace Helium {
 
   }
 
+  /**
+   * @brief Perform a depth first search (DFS).
+   *
+   * This function makes use of a visitor functor to allow actions to be
+   * performed. A number of visitors are available (e.g. DFSAtomOrderVisitor,
+   * DFSBondOrderVisitor, DFSClosureRecorderVisitor, DFSDebugVisitor). Custom
+   * functor can be implemented by inheriting the DFSVisitor struct and
+   * reimplementing the required functions.
+   *
+   * @param mol The molecule.
+   * @param visitor The DFS visitor functor.
+   */
   template<typename MoleculeType, typename DFSVisitorType>
   void depth_first_search(MoleculeType &mol, DFSVisitorType &visitor)
   {
@@ -79,75 +176,175 @@ namespace Helium {
     }
   }
 
-
+  /**
+   * @brief A DFS visitor that records the order in which the atoms are visited.
+   */
   template<typename MoleculeType>
   struct DFSAtomOrderVisitor : public DFSVisitor<MoleculeType>
   {
+    /**
+     * @brief The atom type.
+     */
     typedef typename molecule_traits<MoleculeType>::atom_type atom_type;
 
+    /**
+     * @brief Visit an atom.
+     *
+     * This function records the atom index.
+     *
+     * @param mol The molecule.
+     * @param prev The previous atom on the DFS path.
+     * @param atom The traversed atom.
+     */
     void atom(MoleculeType &mol, atom_type prev, atom_type atom)
     {
       atoms.push_back(get_index(mol, atom));
     }
 
+    /**
+     * @brief The list of atom indices.
+     */
     std::vector<Index> atoms;
   };
 
+  /**
+   * @brief A DFS visitor that records the order in which the bonds are visited.
+   */
   template<typename MoleculeType>
   struct DFSBondOrderVisitor : public DFSVisitor<MoleculeType>
   {
+    /**
+     * @brief The atom type.
+     */
     typedef typename molecule_traits<MoleculeType>::atom_type atom_type;
+    /**
+     * @brief The bond type.
+     */
     typedef typename molecule_traits<MoleculeType>::bond_type bond_type;
 
+    /**
+     * @brief Visit a bond.
+     *
+     * This function records the bond index.
+     *
+     * @param mol The molecule.
+     * @param prev The previous atom on the DFS path.
+     * @param bond The traversed bond.
+     */
     void bond(MoleculeType &mol, atom_type prev, bond_type bond)
     {
       bonds.push_back(get_index(mol, bond));
     }
 
+    /**
+     * @brief The list of bond indices.
+     */
     std::vector<Index> bonds;
   };
 
+  /**
+   * @brief A DFS visitor that records the "back" bonds.
+   */
   template<typename MoleculeType>
   struct DFSClosureRecorderVisitor : public DFSVisitor<MoleculeType>
   {
+    /**
+     * @brief The bond type.
+     */
     typedef typename molecule_traits<MoleculeType>::bond_type bond_type;
 
+    /**
+     * @brief Visit a "back" bond.
+     *
+     * This function records the index of the "back" (or closure) bond.
+     *
+     * @param mol The molecule.
+     * @param bond The "back" bond.
+     */
     void back_bond(MoleculeType &mol, bond_type bond)
     {
       back_bonds.push_back(get_index(mol, bond));
     }
 
+    /**
+     * @brief The list of back bonds.
+     */
     std::vector<Index> back_bonds;
   };
 
+  /**
+   * @brief A DFS visitor that prints out debug information.
+   */
   template<typename MoleculeType>
   struct DFSDebugVisitor : public DFSVisitor<MoleculeType>
   {
+    /**
+     * @brief The atom type.
+     */
     typedef typename molecule_traits<MoleculeType>::atom_type atom_type;
+    /**
+     * @brief The bond type.
+     */
     typedef typename molecule_traits<MoleculeType>::bond_type bond_type;
 
+    /**
+     * @brief Visit an atom.
+     *
+     * This function prints the string "atom(i)" to std::cout where i is the
+     * atom index.
+     *
+     * @param mol The molecule.
+     * @param prev The previous atom on the DFS path.
+     * @param atom The traversed atom.
+     */
     void atom(MoleculeType &mol, atom_type prev, atom_type atom)
     {
       std::cout << "atom(" << get_index(mol, atom) << ")" << std::endl;
     }
 
+    /**
+     * @brief Visit a bond.
+     *
+     * This function prints the string "bond(i)" to std::cout where i is the
+     * bond index.
+     *
+     * @param mol The molecule.
+     * @param prev The previous atom on the DFS path.
+     * @param bond The traversed bond.
+     */
     void bond(MoleculeType &mol, atom_type prev, bond_type bond)
     {
       std::cout << "bond(" << get_index(mol, bond) << ")" << std::endl;
     }
 
+    /**
+     * @brief Backtrack.
+     *
+     * This function prints the string "backtrack(i)" to std::cout where i is the
+     * atom index.
+     *
+     * @param mol The molecule.
+     * @param atom The atom.
+     */
     void backtrack(MoleculeType &mol, atom_type atom)
     {
       std::cout << "backtrack(" << get_index(mol, atom) << ")" << std::endl;
     }
 
+    /**
+     * @brief Visit a "back" bond.
+     *
+     * This function prints the string "back_bond(i)" to std::cout where i is the
+     * "back" bond index.
+     *
+     * @param mol The molecule.
+     * @param bond The "back" bond.
+     */
     void back_bond(MoleculeType &mol, bond_type bond)
     {
       std::cout << "back_bond(" << get_index(mol, bond) << ")" << std::endl;
     }
   };
-
-
 
 }
 

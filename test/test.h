a2d2c50a369ed89ab03f71dc990c7ad5ffe271f0
Tim Vandermeersch <tim.vandermeersch@gmail.com>
2013-10-14 00:18:46 +0200

    Add compare_file() to test.h

--- test/test.h
+++ test/test.h
@@ -1,7 +1,35 @@
+/*
+ * Copyright (c) 2013, Tim Vandermeersch
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #ifndef HELIUM_TEST_H
 #define HELIUM_TEST_H
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <cstdlib>
 
 #ifdef _MSC_VER
@@ -44,6 +72,24 @@ const char* __test_expr__(const char *expr) { return expr; }
 std::string datadir()
 {
   return HEDATADIR + std::string("/");
+}
+
+inline void compare_file(const std::string &filename, const std::string &exp)
+{
+  std::ifstream ifs(filename.c_str());
+  ASSERT(ifs);
+
+  std::string line;
+  std::stringstream correct;
+  while (std::getline(ifs, line))
+    correct << line << std::endl;
+
+  COMPARE(correct.str(), exp);
+}
+
+inline void compare_file(const std::string &filename, const std::stringstream &exp)
+{
+  compare_file(filename, exp.str());
 }
 
 #endif

5c73b1e5173b50b67be83cb43cfe91885e5b23ca
Tim Vandermeersch <tim.vandermeersch@gmail.com>
2013-10-14 23:05:30 +0200

    Add more tests for smiles.h

--- test/smiles.cpp
+++ test/smiles.cpp
@@ -9,12 +9,22 @@ int main()
 {
   HeMol mol;
 
-  try {
-    parse_smiles("c1ccccc1", mol);
-  }
-  catch(Smiley::Exception &e) {
-    std::cerr << e.what();
-  }
+  hemol_from_smiles("CC", mol);
+  COMPARE(2, num_atoms(mol));
+  COMPARE(1, num_bonds(mol));
+  COMPARE(6, get_element(mol, get_atom(mol, 0)));
+  COMPARE(6, get_element(mol, get_atom(mol, 1)));
+  COMPARE(1, get_order(mol, get_bond(mol, 0)));
+
+  hemol_from_smiles("C=C", mol);
+  COMPARE(2, num_atoms(mol));
+  COMPARE(1, num_bonds(mol));
+  COMPARE(6, get_element(mol, get_atom(mol, 0)));
+  COMPARE(6, get_element(mol, get_atom(mol, 1)));
+  COMPARE(2, get_order(mol, get_bond(mol, 0)));
+  COMPARE(2, get_bond(mol, 0).order());
+
+  hemol_from_smiles("c1ccccc1", mol);
   COMPARE(6, mol.numAtoms());
   COMPARE(6, mol.numBonds());
 }
